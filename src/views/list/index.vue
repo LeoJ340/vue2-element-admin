ea51137afd88dd0117ac9f6a989cb889d3940164
LeoJ340 <1015668003@qq.com>
2022-10-28 12:41:51 +0800

    upload image

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app-container">
-    <ImageList :images="list" />
+    <el-row>
+      <input ref="upload" type="file" multiple accept="image/*" style="display: none;" @change="upload">
+      <el-button size="small" type="primary" @click="$refs.upload.click()">点击上传</el-button>
+      <el-button type="danger" size="small">删除</el-button>
+    </el-row>
+    <ImageList v-loading="loading" :images="list" />
   </div>
 </template>
 
@@ -11,6 +16,7 @@ export default {
   components: { ImageList },
   data() {
     return {
+      loading: false,
       list: []
     }
   },
@@ -19,6 +25,7 @@ export default {
   },
   methods: {
     getData() {
+      this.loading = true
       this.$oss.getList().then(res => {
         this.list = res.objects.map((item, index) => {
           return {
@@ -28,7 +35,21 @@ export default {
             tags: ['test', 'hhh']
           }
         })
+      }).finally(_ => {
+        this.loading = false
       })
+    },
+    async upload(e) {
+      const files = Array.from(e.target.files)
+      files.forEach(file => {
+        file.uid = Date.now()
+      })
+      if (files.map(file => file.type.includes('image')).length !== files.length) {
+        this.$message.warning('仅支持图片格式')
+        return
+      }
+      await this.$oss.uploadList(files)
+      this.getData()
     }
   }
 }
