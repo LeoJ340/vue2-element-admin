718230f4fa7b067d275e49631987e1dd300d91cf
LeoJ340 <1015668003@qq.com>
2022-10-28 14:26:43 +0800

    remove image

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -3,9 +3,9 @@
     <el-row>
       <input ref="upload" type="file" multiple accept="image/*" style="display: none;" @change="upload">
       <el-button size="small" type="primary" @click="$refs.upload.click()">点击上传</el-button>
-      <el-button type="danger" size="small">删除</el-button>
+      <el-button type="danger" size="small" @click="remove">删除</el-button>
     </el-row>
-    <ImageList v-loading="loading" :images="list" />
+    <ImageList ref="imageList" v-loading="loading" :images="list" />
   </div>
 </template>
 
@@ -41,15 +41,34 @@ export default {
     },
     async upload(e) {
       const files = Array.from(e.target.files)
-      files.forEach(file => {
-        file.uid = Date.now()
-      })
       if (files.map(file => file.type.includes('image')).length !== files.length) {
         this.$message.warning('仅支持图片格式')
         return
       }
-      await this.$oss.uploadList(files)
-      this.getData()
+      let tempIndex = 0
+      files.forEach(file => {
+        file.uid = `${Date.now()}${tempIndex++}`
+      })
+      const res = await this.$oss.uploadList(files)
+      if (res.length === files.length) {
+        this.$message.success('上传成功')
+        this.getData()
+      }
+    },
+    remove() {
+      const names = this.$refs.imageList.selected.map(image => image.name)
+      this.$confirm('此操作将永久删除文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$oss.removeList(names).then(res => {
+          if (res.res.statusCode === 200) {
+            this.$message.success('删除成功')
+            this.getData()
+          }
+        })
+      }).catch(() => {})
     }
   }
 }
