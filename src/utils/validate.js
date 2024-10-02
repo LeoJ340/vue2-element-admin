8fa803c39b06e00b0d5bb81c267e8db038d5b851
LeoJ340 <1015668003@qq.com>
2024-10-02 20:32:50 +0800

    validator

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -18,3 +18,53 @@ export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
 }
+
+/**
+ * 邮编校验
+ * @param postalCode
+ * @returns {boolean}
+ */
+function post(postalCode) {
+  return /^\d{6}$/.test(postalCode)
+}
+
+/**
+ * 邮箱地址校验
+ * @param email
+ * @returns {boolean}
+ */
+function email(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  return emailRegex.test(email)
+}
+
+/**
+ * 电话校验
+ * @param str
+ * @returns {boolean}
+ */
+function phoneNumber(str) {
+  const mobileRegex = /^1[3-9]\d{9}$/
+  const landlineRegex = /^\d{1,3}[- ]?\d{7,8}$/
+  return mobileRegex.test(str) || landlineRegex.test(str)
+}
+
+/**
+ * 中文校验
+ * @param str
+ * @returns {boolean}
+ */
+function Chinese(str) {
+  return /[\u4e00-\u9fa5]/.test(str)
+}
+
+export const validator = {
+  install(Vue) {
+    Vue.prototype.$validator = {
+      post,
+      email,
+      phoneNumber,
+      Chinese
+    }
+  }
+}
