73691a53a27f5489bc7e592275a109ab79cdf4cf
LeoJ340 <1015668003@qq.com>
2024-10-16 14:07:27 +0800

    信息脱敏 utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -42,13 +42,14 @@ export function parseTime(time, cFormat) {
     s: date.getSeconds(),
     a: date.getDay()
   }
-  const time_str = format.replace(/{([ymdhisa])+}/g, (result, key) => {
+  return format.replace(/{([ymdhisa])+}/g, (result, key) => {
     const value = formatObj[key]
     // Note: getDay() returns 0 on Sunday
-    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value]
+    }
     return value.toString().padStart(2, '0')
   })
-  return time_str
 }
 
 /**
@@ -109,9 +110,73 @@ export function param2Obj(url) {
     const index = v.indexOf('=')
     if (index !== -1) {
       const name = v.substring(0, index)
-      const val = v.substring(index + 1, v.length)
-      obj[name] = val
+      obj[name] = v.substring(index + 1, v.length)
     }
   })
   return obj
+}
+
+/**
+ * @desc 字符串脱敏的操作： 对身份证号和手机号，账号进行脱敏操作
+ * @param value 注意: 需要脱敏的值,要求传入的是未格式化的原值
+ * @param symbol 用来脱敏的特殊字符
+ * @return {string} 返回脱敏后的值 先脱敏后再进行格式化
+ * @param type 区分手机号：M，账号 A，身份证号 C, 默认是账号
+ */
+
+export function mask(value = '', type = 'A', symbol = '*') {
+  if (value) {
+    // 判断如果已经进行了脱敏则不再进行脱敏
+    if (value.toString().includes('*') || value.toString().includes('#')) {
+      return value
+    }
+    // 替换掉所有的空格
+    value = value.replace(/\s/g, '')
+
+    // 手机号脱敏
+    if (type === 'M') {
+      // 默认11位
+      if (value.length >= 7) {
+        return value
+          .toString()
+          .replace(/(\d{3})\d*(\d{4})/, '$1 **** $2')
+          .replace(/\*/g, symbol)
+      }
+      return value
+    }
+
+    // 账号脱敏 前4位数字，后4位数字，中间都是8位脱敏符号：如**** ****    示例：  6270 **** **** 2375
+    if (type === 'A') {
+      if (value.length >= 8) {
+        return value
+          .toString()
+          .replace(/(\d{4})\d*(\d{4})/, '$1 **** **** $2')
+          .replace(/\*/g, symbol)
+      }
+      return value
+    }
+
+    //  6270 **** 2375
+    if (type === 'A1') {
+      if (value.length >= 8) {
+        return value
+          .toString()
+          .replace(/(\d{4})\d*(\d{4})/, '$1 **** $2')
+          .replace(/\*/g, symbol)
+      }
+      return value
+    }
+
+    // 身份证号脱敏
+    if (type === 'C') {
+      if (value.length >= 7) {
+        return value
+          .toString()
+          .replace(/(\d{4})\d*(\d{3})/, '$1***********$2')
+          .replace(/\*/g, symbol)
+      }
+      return value
+    }
+  }
+  return value
 }
