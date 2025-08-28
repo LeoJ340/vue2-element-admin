fde87c5d4870291c6cf4b65c503030b3a1758402
LeoJ340 <1015668003@qq.com>
2025-08-28 08:19:07 +0800

    add downloadText function

--- src/utils/download.js
+++ src/utils/download.js
@@ -41,3 +41,29 @@ export function OneByOne(imageUrls) {
     }, 5 * 60 * 1000)
   })
 }
+
+/**
+ * 下载文本文件
+ * @param content
+ * @param filename
+ */
+export function downloadText(content, filename = 'download.txt') {
+  // 创建一个 Blob 对象
+  const blob = new Blob([content], { type: 'text/plain' })
+  // 创建一个临时的 <a> 标签
+  const a = document.createElement('a')
+  // 创建一个 URL 指向 Blob 对象
+  const url = URL.createObjectURL(blob)
+  // 设置 <a> 标签的 href 属性为 Blob URL
+  a.href = url
+  // 设置 <a> 标签的 download 属性为文件名
+  a.download = filename
+  // 将 <a> 标签添加到文档中（虽然这里我们不会真正显示它）
+  document.body.appendChild(a)
+  // 模拟点击 <a> 标签以触发下载
+  a.click()
+  // 移除 <a> 标签
+  document.body.removeChild(a)
+  // 释放 Blob URL
+  URL.revokeObjectURL(url)
+}
